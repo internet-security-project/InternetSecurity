475e984da6964a62322976ebb42dafb413d5cdb3
Jihee Park <applepjh7@naver.com>
2024-10-28 23:18:17 +0900

    login

--- login/app.py
+++ login/app.py
@@ -71,4 +71,4 @@ def login():
         return jsonify({'message': '로그인 실패'}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
